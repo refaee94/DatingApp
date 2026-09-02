38310364b13e67c824a5a7409547fbc986c5015b
memo <m.refaee123580@gmail.com>
2026-09-02 23:50:06 +0400

    Initialize API project structure with essential files and configurations

---
+++ API/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+
+
+using API.Entites;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class AppDbContext(DbContextOptions options) : DbContext(options)
+    {
+        
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
---
+++ API/Entites/AppUser.cs
@@ -0,0 +1,12 @@
+
+
+namespace API.Entites
+{
+    public class AppUser
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public required string DisplayName { get; set; }
+        public required string Email { get; set; }
+
+    }
+}
