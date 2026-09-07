0bd958447fefaa33d25239649e96c1323b25416c
memo <m.refaee123580@gmail.com>
2026-09-08 02:27:53 +0400

    feat(API): add error handling middleware and endpoints for server errors, not found, and unauthorized access feat(client): implement error interceptor and create error handling components for server and not found errors

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,6 @@
 using API.Data;
 using API.Interfaces;
+using API.Middelware;
 using API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
@@ -30,7 +31,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJw
    
 var app = builder.Build();
 
-
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200","http://localhost:4200"));  
 app.UseAuthentication();
 app.UseAuthorization();
